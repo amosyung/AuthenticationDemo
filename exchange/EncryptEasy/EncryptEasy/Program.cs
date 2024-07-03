ce8b9d75279f760b9ec55628fe23aaf71b370221
amosyung <amosyung@hotmail.com>
2024-07-03 17:15:15 -0400

    update

--- exchange/EncryptEasy/EncryptEasy/Program.cs
+++ exchange/EncryptEasy/EncryptEasy/Program.cs
@@ -1,24 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
-string action = args[0];
-string sourcePath = args[1];
-string destinationPath = args[2];
-string password = args[2];
+class Program {
 
-/*
-string action = "-e";
-string sourcePath = "C:\\Projects\\MPCE\\mpce.zip";
-string destinationPath = "C:\\temp\\mpce_go\\mpce_zip";
-string password = "demolition";
-*/
-/*
-string action = "-d";
-string sourcePath = "C:\\temp\\mpce_go\\mpce_zip";
-string destinationPath = "c:\\temp\\mpce_go\\mpce.zip";
-string password = "demolition";
-*/
-if (action == "-e") 
-EncryptEasy.Processor.Encrypt(sourcePath, destinationPath, password);
-else if (action == "-d")    
-EncryptEasy.Processor.Decrypt(sourcePath, destinationPath, password);
+    public static void Main(string[] args) {
+
+    string action = args[0];
+    string sourcePath = args[1];
+    string destinationPath = args[2];
+    string password = args[3];
+
+    if (action == "-e") 
+    EncryptEasy.Processor.Encrypt(sourcePath, destinationPath, password);
+    else if (action == "-d")    
+    EncryptEasy.Processor.Decrypt(sourcePath, destinationPath, password);
+    }
+}
